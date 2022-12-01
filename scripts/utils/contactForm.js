e7a4f0392146bceae770d80f305e24ab31c143f0
Dakusy <dakusy@gmail.com>
2022-12-01 22:03:04 +0100

    add verification on modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -2,7 +2,6 @@ const modal = document.getElementById("contact_modal");
 
 function displayModal() {
 	modal.style.display = "block";
-    document.getElementById("firstName").focus();
 }
 
 function closeModal() {
@@ -10,25 +9,70 @@ function closeModal() {
     modal.style.display = "none";
 }
 
+/* Affiche le nom du photographe dans le modal.*/
+
 async function putUserName(photographe) {
     const name = document.querySelector(".modal header h2");
     name.innerHTML = `Contacter-moi</br>
-    ${photographe}`;
+    ${photographe.name}`;
     
 }
+
+/*Permet de récupérer les infos du photographe depuis l'ID.*/
 
 async function initName() {
     const id = await getUrlID();
     const photographe = JSON.parse(localStorage.getItem(id));
-    await putUserName(photographe.name);
+    await putUserName(photographe);
   }
 
   initName();
 
   // Variables //
 
-  const firstName = document.getElementById("firstName");
-  const lastName = document.getElementById("lastName");
-  const email = document.getElementById("email");
-  const texte = document.getElementById("texte");
-  const nameContact = document.querySelector(".modal header h1");
+  const firstName = document.getElementById("prenom");
+  const lastName = document.getElementById("nom");
+  const email = document.getElementById("mail");
+  const message = document.getElementById("message");
+
+
+//Vérification prénom/nom fonction
+const checkName=(name, type) =>{
+    const regText = /[a-zA-ZÀ-ÿ]{2}/;
+    if (regText.test(name.value)) {
+      name.parentNode.dataset.errorVisible = false;
+      return true
+    } else {
+      name.parentNode.dataset.error = `Le ${type} doit avoir 2 caractères ou plus`;
+      name.parentNode.dataset.errorVisible = true;
+      return false
+    }
+  };
+
+  //Event si le le prénom est mauvais
+firstName.addEventListener('blur', function () {
+    checkName(firstName,"prénom");
+  });
+  
+  //Event si le nom est mauvais
+lastName.addEventListener('blur', function () {
+    checkName(lastName,"nom");
+  });
+
+//Vérification email fonction
+const checkEmail=(email) =>{
+    const regEmail = /^[\w\-\+]+(\.[\w\-]+)*@[\w\-]+(\.[\w\-]+)*\.[\w\-]{2,4}$/;
+    if (regEmail.test(email.value)) {
+      email.parentNode.dataset.errorVisible = false;
+      return true
+    } else {
+      email.parentNode.dataset.error = "Veuillez entrer un mail valide";
+      email.parentNode.dataset.errorVisible = true;
+      return false
+    }
+  };
+
+  email.addEventListener('blur', function () {
+    checkEmail(email);
+  });
+
